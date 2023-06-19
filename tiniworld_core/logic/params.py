fb53ee006031519feed6bf71dfaa845c74d96d4d
David Gilles <info@davidgilles.de>
2023-06-19 16:12:18 +0200

    move THESHOLD to .env

--- tiniworld_core/logic/params.py
+++ tiniworld_core/logic/params.py
@@ -3,8 +3,7 @@ tiniworld model package params
 load and validate the environment variables in the `.env`
 """
 import os
-import numpy as np
 
 LOCAL_DATA_PATH='raw_data'
 LOCAL_MODEL_PATH='model'
-THRESHOLD = 2500
+THRESHOLD = os.environ.get('THRESHOLD', 3500)
