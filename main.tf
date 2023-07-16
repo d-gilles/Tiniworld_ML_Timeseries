760e74682e68bbbc19d4adc8b176fcf96f4295eb
David Gilles <info@davidgilles.de>
2023-07-16 16:44:11 +0200

    add disable_dependent_services

--- main.tf
+++ main.tf
@@ -7,9 +7,11 @@ provider "google" {
 resource "google_project_service" "container_registry" {
   project = var.project
   service = "storage-component.googleapis.com"
+  disable_dependent_services=true
 }
 
 resource "google_project_service" "cloud_run" {
   project = var.project
   service = "run.googleapis.com"
+  disable_dependent_services=true
 }
